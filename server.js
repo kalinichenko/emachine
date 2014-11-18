d4aaa6b1f613dee54efb5ddd00a3500d2d632641
Andrey Kalinichenko <kalinichenko@bifit.ua>
2014-11-18 17:23:23 +0200

    typo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('./app');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var server = app.listen(app.get('port'), app.get('address'), function() {
+var server = app.listen(server_port, server_ip_address, function() {
   console.log("Listening on " + server_ip_address + ", server_port " + port);
 });
+
