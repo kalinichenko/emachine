29d0deee80a95a857619283402cbe33be34ebdfe
Andrey Kalinichenko <kalinichenko@bifit.ua>
2014-11-18 17:07:10 +0200

    fix typo

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app = require('../app');
+var app = require('app');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
