e57386f39268f252bca5dd1082ac0ba147aeb853
Andrey Kalinichenko <kalinichenko@bifit.ua>
2014-11-26 17:02:10 +0200

    cors support was added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var db = require('./db');
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Repeat after me'});
